87b83e6f91d761ae38a8968f4e6d2eabd8c8bdb7
shreyT19 <shreyanshtripathy49@gmail.com>
2023-02-14 20:09:26 +0530

    changes

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -73,7 +73,7 @@ const Footer = () => {
                         <div className="foot_copyright">
                             <p>
                                 {currYear} | XBeat. All Rights Reserved.
-                                Built by | <a href="https://gulshansongara.netlify.app/">Gulshan Songara</a>
+                                Built by | <a href="#">Shreyansh Tripathy</a>
                             </p>
                         </div>
                         <div className="foot_social">
